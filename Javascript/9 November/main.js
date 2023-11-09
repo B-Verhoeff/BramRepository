2a0296a0bff61224cd9c42a0be2fbac8c39e90dd
B-Verhoeff <143403242+B-Verhoeff@users.noreply.github.com>
2023-11-09 12:54:42 +0100

    9 november

---
+++ Javascript/9 November/main.js
@@ -0,0 +1,14 @@
+console.log("Het werkt");
+
+//opdr 1
+let oldSalaris = 2000;
+let multiPlyer = 2000 * 0.05;
+let newSalaris = oldSalaris + multiPlyer;
+console.log(newSalaris);
+
+//opdr 2
+let text = document.querySelector(".newPrice");
+let prijs = 400000
+let percentage = 400000 * 0.025;
+let newPrice = prijs + percentage;
+document.getElementById("newPrice").innerHTML
