aca939abd4c30d1b9856f7d0c3a454c757fb5ab5
B-Verhoeff <302644473@student.rocmondriaan.nl>
2023-11-10 12:13:49 +0100

    habiba

--- Javascript/9 November/main.js
+++ Javascript/9 November/main.js
@@ -66,3 +66,116 @@ if(crypto === "BTC") {
     result = (1/40.26)*euros;
     console.log("Solerium: ", result);
 }
+
+//opdr 6
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+}
+console.log("Einde programma");
+ 
+//opdr 7
+let count = 0;
+ 
+while (count < 20) {
+    if (count < 10) {
+        console.log("Dit is zin nummer 1");
+    } else {
+        console.log("Dit is zin nummer 2");
+    }
+    count++;
+}
+console.log("Einde programma");
+
+//opdr 8
+count = 0;
+ 
+while (count < 20) {
+    console.log("Loop");
+    count++;
+ 
+    if (count === 10) {
+        break;
+    }
+}
+//opdr 9
+let kleuren = ["rood", "groen", "geel", "blauw", "roze"];
+ 
+for (let kleur of kleuren) {
+    document.write(`<p>${kleur}</p>`);
+}
+ 
+//opdr 10
+let muziekArtiesten = ["Artiest 1", "Artiest 2", "Artiest 3", "Artiest 4", "Artiest 5"];
+ 
+function voegArtiestToe() {
+    let randomIndex = Math.floor(Math.random() * muziekArtiesten.length);
+    let div = document.createElement("div");
+    div.textContent = muziekArtiesten[randomIndex];
+    document.body.appendChild(div);
+}
+ 
+//opdr 11
+let tekstArray = [];
+ 
+function voegToe() {
+    let inputTekst = document.getElementById("tekstInput").value;
+    tekstArray.push(inputTekst);
+    updateUl();
+}
+ 
+function wisAlles() {
+    tekstArray = [];
+    updateUl();
+}
+ 
+function updateUl() {
+    let ul = document.getElementById("tekstUl");
+    ul.innerHTML = "";
+    tekstArray.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item;
+        ul.appendChild(li);
+    });
+}
+ 
+//opdr 12
+function showMessage(bericht) {
+    console.log(bericht);
+}
+ 
+showMessage("Dit is een voorbeeldbericht.");
+ 
+//opdr 13
+function showSum(a, b) {
+    console.log(a + b);
+}
+ 
+showSum(5, 10);
+ 
+//opdr 14
+function showArray(arr) {
+    for (let item of arr) {
+        console.log(item);
+    }
+}
+ 
+showArray(["item1", "item2", "item3"]);
+ 
+//opdr 15
+function getProduct(a, b) {
+    return a * b;
+}
+ 
+console.log(getProduct(5, 10));
+ 
+//opdr 16
+function getSumOfArray(arr) {
+    let sum = 0;
+    for (let item of arr) {
+        sum += item;
+    }
+    return sum;
+}
+ 
+let resultaat = getSumOfArray([1, 3, 5, 7, 11]);
+console.log(resultaat);
