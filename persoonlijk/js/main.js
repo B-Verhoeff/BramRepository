9b4c46937e4e6696bd3b7b07b71c395a502bd491
B-Verhoeff <302644473@student.rocmondriaan.nl>
2023-11-29 14:28:35 +0100

    dadsads

---
+++ persoonlijk/js/main.js
@@ -0,0 +1,13 @@
+console.log("main.js is running...")
+let a = 5;
+let b = 9;
+let c = a * b;
+console.log(a * b);
+console.log(c)
+
+if (prompt = 123) {
+    alert("vuile flikker")
+}
+else {
+    alert("suriname")
+}
