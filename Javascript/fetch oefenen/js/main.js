eccc584c782e2fe950d2e7c9d3a0699729814dbf
B-Verhoeff <ultrawebshop@gmail.com>
2025-04-10 16:51:27 +0200

    xcz

--- Javascript/fetch oefenen/js/main.js
+++
@@ -1,8 +0,0 @@
-console.log("Bram")
-
-const shop_url = "https://dummyjson.com/products?limit=4"
-fetch(shop_url)
-.then((response) => response.json())
-.then((data) => {
-    console.log("Ontvangen data: ", data);
-});
